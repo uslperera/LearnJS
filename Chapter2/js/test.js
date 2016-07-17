b38b11fc1c2fdbc6bd901a671eb42ee9d9e9af7f
Shamal <shamal@Shamals-MacBook-Pro.local>
2016-07-17 19:23:12 +0530

    Literals, Variables, Constants & Data Types

---
+++ Chapter2/js/test.js
@@ -0,0 +1,35 @@
+// const name = "shamal"
+// const multiline = "line1 ${name}\
+// line2";
+// console.log(multiline);
+
+
+// const RED = Symbol("red");
+// const RED1 = Symbol("red");
+// console.log(RED === RED1);
+
+
+// const obj = {};
+// obj.size=5;
+// console.log(obj.size);
+// obj["not an identifier"]=3;
+// console.log(obj)
+// const SIZE = Symbol();
+// obj[SIZE] = 8;
+// console.log(obj[SIZE]);
+// console.log(obj.SIZE);
+
+
+// const s = "hello";
+// console.log(s.toUpperCase());
+// s.rating = 4;
+// console.log(s.rating);
+
+
+// const arr = ['a', 1, {name: 'shamal', age: 23}];
+// console.log(arr.length);
+
+const b =0;
+console.log(!!b);
+const b2 = Boolean(b);
+console.log(b2);
--- Chapter2/public/es6/test.js
+++ Chapter2/public/es6/test.js
@@ -1,9 +1,9 @@
-'use strict'
+'use strict';
 // es6 feature: block-scoped "let" declaration
 const sentences = [
 	{ subject: 'JavaScript', verb: 'is', object: 'great'},
 	{ subject: 'Elephants', verb: 'are', object: 'large'}
-]
+];
 // es6 feature: object destructuring
 function say({subject, verb, object}){
 	//es6 feature: template strings
